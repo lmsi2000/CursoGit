344c37a8f2f8b5091835350b4df6d2faccb39a12
David Neururer <dargor24@gmail.com>
2020-10-30 18:50:40 +0100

    "Primer commit"

---
+++ Mes.java
@@ -0,0 +1,18 @@
+public class Mes{
+	public static void main(String[] args){
+		int mes=11; totalDias;
+		switch (mes){
+			case 1:
+					mes==1||mes==3||mes==5||mes==7||mes==8||mes==10||mes==12;
+					totalDias=("Mes: "+mes+" tiene "+totalDias+" dias");
+					break;
+			case 2:
+					mes==2;
+					totalDias=("Mes: "+mes+" tiene "+totalDias+" dias");
+					break;
+			default:
+					totalDias=("Mes: "+mes+" tiene "+totalDias+" dias");
+					break;
+		}
+	}
+}	
